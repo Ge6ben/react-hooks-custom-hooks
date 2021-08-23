6672f5ffb76e096d783168ba1cff86c6c6b8a846
Ge6ben <dl22gesh40@gmail.com>
2021-08-23 10:43:29 +0300

    check

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import About from "./About";
 import ArticleList from "./ArticleList";
+import useDocumentTitle from "../hooks/useDocumentTitle";
 
 function HomePage() {
   // fetch data for posts
@@ -16,12 +17,12 @@ function HomePage() {
         setIsLoaded(true);
       });
   }, []);
-
+  const pageTitle = "Underreacted | Home";
   // set the document title
-  useEffect(() => {
-    document.title = "Underreacted | Home";
-  }, []);
-
+  useEffect((pageTitle) => {
+    document.title = pageTitle;
+  }, [pageTitle]);
+  useDocumentTitle("Underreacted | Home");
   return (
     <>
       <About />
